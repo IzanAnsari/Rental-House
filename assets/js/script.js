9267069b8249f385a614ec6cff5a51603620062f
Ansari Mohammad Izan <izanansari123@gmail.com>
2024-04-21 01:33:40 +0530

    smfs

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,53 @@ for (let i = 0; i < navbarLinks.length; i++) {
 
 const header = document.querySelector("[data-header]");
 
+document.addEventListener("DOMContentLoaded", function() {
+  const registerBtn = document.querySelector(".register-btn");
+  const userBtn = document.querySelector(".user-btn");
+  
+  // Check localStorage on page load
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (isLoggedIn === "true") {
+    // Change register icon and text
+    registerBtn.innerHTML = '<ion-icon name="log-out-outline"></ion-icon><span id="register-label">Logout</span>';
+    // Hide user icon
+    userBtn.style.display = "none";
+  }
+  
+  registerBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    // Toggle login/logout state
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (isLoggedIn === "true") {
+      // Logout
+      localStorage.setItem("isLoggedIn", "false");
+      // Change register icon and text
+      registerBtn.innerHTML = '<ion-icon name="car-outline"></ion-icon><span id="register-label">Register</span>';
+      // Show user icon
+      userBtn.style.display = "inline-block";
+    } else {
+      // Login
+      localStorage.setItem("isLoggedIn", "true");
+      // Change register icon and text
+      registerBtn.innerHTML = '<ion-icon name="log-out-outline"></ion-icon><span id="register-label">Logout</span>';
+      // Hide user icon
+      userBtn.style.display = "none";
+    }
+    
+  });
+  
+  userBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    // Check if user is logged in
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (isLoggedIn === "true") {
+      // Clear localStorage
+      localStorage.clear();
+      // Change user icon
+      userBtn.style.display = "inline-block";
+    } else {
+      // Redirect to login page
+      window.location.href = "./view/login.html";
+    }
+  });
+});
