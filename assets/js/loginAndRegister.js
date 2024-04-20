9a1ed6b81ffeb3d7f424fca1b59228d7f4359ad7
Ansari Mohammad Izan <izanansari123@gmail.com>
2024-04-20 13:25:57 +0530

    first

---
+++ assets/js/loginAndRegister.js
@@ -0,0 +1,38 @@
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait for the DOM content to be fully loaded
+    document.getElementById("loginButton").addEventListener("click", function(event) {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+        
+        // Redirect to index.html when the button is clicked
+        window.location.href = "../index.html";
+    });
+});
